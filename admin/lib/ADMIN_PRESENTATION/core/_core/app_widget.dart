d1f00e137d010d0cbae7a95e16fc023c0795634e
Alban Rochet <>
2023-06-23 10:32:43 +0200

    [Nom] Changement pour Météo Orkester

--- admin/lib/ADMIN_PRESENTATION/core/_core/app_widget.dart
+++ admin/lib/ADMIN_PRESENTATION/core/_core/app_widget.dart
@@ -26,7 +26,7 @@ class AppWidget extends StatelessWidget {
     );
 
     return MaterialApp.router(
-      title: 'Base de Projet',
+      title: 'Météo Orkester',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         //COLORS
